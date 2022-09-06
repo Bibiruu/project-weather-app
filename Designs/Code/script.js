11b8948dff2a75534da693913eb443b7f20a0091
Maria <so.mariagloria@gmail.com>
2022-09-06 21:28:34 +0300

    first commit

---
+++ Designs/Code/script.js
@@ -0,0 +1,24 @@
+
+const currentWeather = document.getElementById('currentWeather')
+const currentCity = document.getElementById('currentCity')
+const currentdate = document.getElementById('currentDate')
+const button = document.getElementById('button')
+
+/*fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=d7e64bc8f54b920fd57db6615f56c622")
+.then((response) =>{
+    return response.json()
+})
+.then((json) =>{
+
+    container.innerHTML += `<p>Inject : ${json.id} and ${json.name}<p>`
+})
+.catch((error) =>{
+    console.log("You have an error", error)
+})
+
+const test = document.getElementById('test')*/
+
+
+
+//evenlistener draft
+//button.addEventListener('click' () =>{})
